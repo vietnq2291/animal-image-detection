fcc66609d579ae65eb04e20829f0dbccb329abdf
Ngo Quang Viet <viet.nq194881@sis.hust.edu.vn>
2022-11-13 10:10:43 +0700

    init

---
+++ input/animals10/translate.py
@@ -0,0 +1,2 @@
+translate = {"cane": "dog", "farfalla": "butterfly", "gallina": "chicken", "gatto": "cat"}
+# translate = {"cane": "dog", "cavallo": "horse", "elefante": "elephant", "farfalla": "butterfly", "gallina": "chicken", "gatto": "cat", "mucca": "cow", "pecora": "sheep", "scoiattolo": "squirrel", "dog": "cane", "cavallo": "horse", "elephant" : "elefante", "butterfly": "farfalla", "chicken": "gallina", "cat": "gatto", "cow": "mucca", "spider": "ragno", "squirrel": "scoiattolo"}
